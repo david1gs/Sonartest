c61d0a95af91a197eb7853fb97a1a9f03043ab12
david <david@localhost.localdomain>
2018-03-21 19:08:52 -0400

    192 - 185

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -2,53 +2,80 @@ package com.devops.app;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.chrono.HijrahDate;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-        public static void main(String[] args) {
-        // 1.- 
-         
-        AtomicInteger aInt1 =  new AtomicInteger(0);
-        AtomicInteger aInt2 =  new AtomicInteger(0);
+public class App {
+	String name;
 
-        if( aInt1.equals(aInt2) ){
-            
+    public static void main(String[] args) {
+   		test_divide();
+        weekYear();
+   		setName("Lalo");
+    }
+
+    //292 .- 
+    public static void test_divide(){
+    	int a = 0;
+    	int z = 0;
+    	if( a > 0 ){
+    		z = 1;
+    	} 
+    	if (a < 0){
+    		z = 3;
+    	}
+
+    	z = 1 / z;
+    }    
+
+    //291
+    public static void weekYear(){
+    	try {
+            Date date = new SimpleDateFormat("yyyy/MM/dd").parse("2015/12/31");
+ 			String result = new SimpleDateFormat("YYYY/MM/dd").format(date);  	
+    		System.out.println(result);
+        } catch (ParseException ex) {
+            //Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        // 2.-
-        int target  = 5;
-        int num     = 3;
-        
-        target =- num;
-        target =+ num;
-        
-        //3.- 
-        Test3();
-        
-    }
-    
-    //3.-
-    @Deprecated
-    public static void Test3(){
     
     }
 
+    //289
+    public static void setName(String name){
+    	name = name;
+    }
+
+    //288
+    public static int pickNumber(){
+    	int i = 0;
+    	int j = 0;
+    	i = i++;
+
+    	return j++;
+    }
+
+    
 }
 
 
-//5.-
-class ParentClass{
-    public boolean doSomething(){
-        return false;
-    }
+//287
+class MyClass2 implements Serializable{
+	private HijrahDate date; // Noncompliant; mark this transient
+	//private transient HijrahDate date;
 }
 
-class firstChildClass extends ParentClass{
-    public boolean doSomething(){
-        return true;
-    }
+
+//285
+class StringUtils{
+	public static String concatenate(String s1, String s2){
+		return s1 + s2;
+	}
 }
